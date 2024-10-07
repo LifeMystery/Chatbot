b07e7b2f00dffa2516e24416f69e79c300767be5
iaphane023 <iaphane023@student.wethinkcode.co.za>
2024-10-07 07:38:51 +0200

    pip install numpy, tensorflow and nltk

---
+++ pract.py
@@ -0,0 +1,11 @@
+#i will make a list of words then check if the word given matches any of them then give a response from that word
+# e.g ["hello", "hy", "hey"] = hello, how may i help you today... 
+# ["is it free", "how much"] = the program is free, but you will not get any stipend
+
+
+def calling_of_a_function_here():
+    print("start here")
+
+
+if __name__ == "__main__":
+    calling_of_a_function_here()
